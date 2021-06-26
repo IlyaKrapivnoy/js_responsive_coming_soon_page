08e85d79937efd1819494853637d062c07210e7f
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-26 12:26:54 +0300

    Display result

--- js/main.js
+++ js/main.js
@@ -16,4 +16,13 @@ const intvl = setInterval(() => {
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display result
+    countdown.innerHTML = `
+    <div>${days} <span>Days</span></div>
+    <div>${hours} <span>Hours</div>
+    <div>${mins} <span>Minutes</div>
+    <div>${seconds} <span>Seconds</div>
+    
+    `;
 }, 1000);
